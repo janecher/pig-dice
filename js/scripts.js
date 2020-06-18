c743d33edeb4b3bdd6399a6df76cfccc52af64fd
James Henager <jameschenager@gmail.com>
2020-06-18 10:09:15 -0700

    add functioning second dice, with altered functions to accommodate it
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -37,17 +37,36 @@ Dice.prototype.roll = function() {
 }
 
 // User Interface
-function showInfo(totalScore1, totalScore2, turnScore1, turnScore2, diceSide) {
+function showInfo(totalScore1, totalScore2, turnScore1, turnScore2, diceSide1, diceSide2) {
   $("div#total-score-1").text(totalScore1);
   $("div#total-score-2").text(totalScore2);
   $("div#turn-score-1").text(turnScore1);
   $("div#turn-score-2").text(turnScore2);
-  $("div#diceResult").text(diceSide);
+  $("div#dice1").text(diceSide1);
+  $("div#dice2").text(diceSide2);
 }
 
-function buttonRollDice (btnPlay1, btnPlay2, btnHold, player, dice) {
-  player.play(dice);
-  if (dice.side === 1) {
+function chooseDiceImage (side) {
+  switch (side) {
+    case (1):
+      return "<img src='one.jpeg'>";
+    case (2):
+      return "<img src='two.jpeg'>";
+    case (3):
+      return "<img src='three.jpeg'>";
+    case (4):
+      return "<img src='four.jpeg'>";
+    case (5):
+      return "<img src='five.jpeg'>";
+    case (6):
+      return "<img src='six.jpeg'>";   
+  }
+}
+
+function buttonRollDice (btnPlay1, btnPlay2, btnHold, player, dice1, dice2) {
+  player.play(dice1);
+  player.play(dice2);
+  if (dice1.side === 1 || dice2.side === 1) {
     $(btnPlay1).hide();
     $(btnHold).hide();
     $(btnPlay2).show();
@@ -74,30 +93,31 @@ function gameStop(total1, total2, player1, player2) {
 }
 
 $(document).ready(function(){
-  let dice = new Dice();
+  let dice1 = new Dice();
+  let dice2 = new Dice();
   let player1 = new Player(0, 0);
   let player2 = new Player(0, 0);
   let scoreWin1;
   let scoreWin2;
 
   $("button#play1").click(function(){
-    buttonRollDice ("#play1", "#play2", "#hold1", player1, dice);
+    buttonRollDice ("#play1", "#play2", "#hold1", player1, dice1, dice2);
     scoreWin1 = player1.scoreTotal + player1.scoreTemp;
-    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice.side);
+    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice1.side, dice2.side);
     gameStop(scoreWin1, scoreWin2, player1, player2);
   });
   $("button#play2").click(function(){
-    buttonRollDice ("#play2", "#play1", "#hold2", player2, dice);
+    buttonRollDice ("#play2", "#play1", "#hold2", player2, dice1, dice2);
     scoreWin2 = player2.scoreTotal + player2.scoreTemp;
-    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice.side);
+    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice1.side, dice2.side);
     gameStop(scoreWin1, scoreWin2, player1, player2);
   });
   $("button#hold1").click(function(){
     buttonHold("#play1", "#play2", "#hold1", player1);
-    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice.side);
+    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice1.side, dice2.side);
   });
   $("button#hold2").click(function(){
     buttonHold("#play2", "#play1", "#hold2", player2);
-    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice.side);
+    showInfo(player1.scoreTotal, player2.scoreTotal, player1.scoreTemp, player2.scoreTemp, dice1.side, dice2.side);
   });
 });
